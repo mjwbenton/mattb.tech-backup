67e0351f7f25e9c5b4e5aa44dcb25ad56e4c9007
Matthew Benton <m@mattb.tech>
2020-04-12 21:44:46 +0100

    Hacky browsing to multiple pages

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,17 @@ const main = async () => {
       console.error(`Request failed: ${request.url()}`)
     );
     await page.goto(WEBSITE, { waitUntil: "networkidle0" });
+    const urls = await page.evaluate(() =>
+      Array.from(document.querySelectorAll("a[href]")).map(
+        (el: HTMLAnchorElement) => el.href
+      )
+    );
+    for (const url of urls) {
+      if (url.startsWith(WEBSITE)) {
+        console.log(`Loading page ${url}`);
+        await page.goto(url, { waitUntil: "networkidle0" });
+      }
+    }
     await browser.close();
   } catch (error) {
     console.error(error);
