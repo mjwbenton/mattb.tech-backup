1bf76d578f8f1ccd1881e34595f0448fa776830b
Matthew Benton <m@mattb.tech>
2020-04-13 11:44:51 +0100

    Disable timeout - we can wait

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ const main = async () => {
     page.on("requestfailed", async request =>
       console.error(`Request failed: ${request.url()}`)
     );
-    await page.goto(WEBSITE, { waitUntil: "networkidle0" });
+    await page.goto(WEBSITE, { waitUntil: "networkidle0", timeout: 0 });
     const urls = await page.evaluate(() =>
       Array.from(document.querySelectorAll("a[href]")).map(
         (el: HTMLAnchorElement) => el.href
@@ -46,7 +46,7 @@ const main = async () => {
     for (const url of urls) {
       if (url.startsWith(WEBSITE)) {
         console.log(`Loading page ${url}`);
-        await page.goto(url, { waitUntil: "networkidle0" });
+        await page.goto(url, { waitUntil: "networkidle0", timeout: 0 });
       }
     }
     await browser.close();
